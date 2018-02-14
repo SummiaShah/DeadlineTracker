72ebd28da4caa0269da2958984df308743a9eda6
xlcr <akito989@hotmail.com>
2018-02-14 05:47:47 +0000

    Linking for registration + bug fix
    1. Registration php script linked (but scripts must be moved to the scripts folder first)
    
    2. Removed single quotes from user_check_query: double quotes can read $variables just fine, and it was causing errors
    
    3. moved mysqli_fetch_assoc only if the previous query's result returns true; if it's false, an error would happen
    
    4. sql insertion of the user, this was omitted?
    
    5. after insertion, user will be directed to dashboard.html (although the file extension might change in the future)
    
    Problems
    Can't link login form; if statements aren't closed; can't tell the intention

--- registration.php
+++ registration.php
@@ -6,18 +6,17 @@ $host = 'localhost';
 $user = 'root';
 $pWord = '';
 $database = 'deadlinetracker';
-$errors = array(); 
+$errors = array();
 
 // Create connection
-$db = mysqli($host, $user, $pWord, $database);
-} 
+$db = mysqli_connect($host, $user, $pWord, $database);
 
 $username = '';
 $password = '';
 $email = '';
 
 // register user
-if (isset($_POST['register'])) {	
+if (isset($_POST['register'])) {
   //register button clicked
   // receive all input values from the form
   $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -27,26 +26,30 @@ if (isset($_POST['register'])) {
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password)) { array_push($errors, "Password is required");  }
-  
+
   // checks username and email are unique
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM users WHERE username=$username OR email=$email LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
- 
- if ($user) { // if user exists
+
+  if ($result) { // if user exists
+    $user = mysqli_fetch_assoc($result);
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
     }
-	
-	if ($user['email'] === $email) { // if email exists
-      array_push($errors, "email already exists");
-    }
+
+  	if ($user['email'] === $email) { // if email exists
+        array_push($errors, "email already exists");
+      }
   }
-  
+
   if (count($errors) == 0) {
 	  $password = md5($password);
-	  $sql = 'INSERT INTO User (Username, Password, Email)
-	  VALUES ('$username', '$password', '$email')';
+	  $sql = "INSERT INTO User (Username, Password, Email)
+	  VALUES ('$username', '$password', '$email')";
+    $result = mysqli_query($db, $sql);
+    header("location: ../dashboard.html");
   }
+
 }
+
 ?>
